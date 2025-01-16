cdf3e56ae2f8ad7546deca043ae8cfadbef0d12e
Gean Segura Sánchez <120821919+GeanSegura@users.noreply.github.com>
2025-01-16 16:46:48 -0500

    GAF-011

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,7 +47,7 @@
                             <img class="d-block img-fluid" src="./img/Fondo2025.png"
                                 alt="First slide">
                             <div class="carousel-caption d-none d-md-block">
-                                <div class="banner-text">
+                                <div class="banner-text-m">
                                     <h2>Sobre Nosotros</h2>
                                     <p>Midhco Distribuciones es una empresa con 20 años en el mercado farmacéutico, siendo líder en la zona centro del Perú y ofreciendo productos de la más alta calidad y certificación. Está guiada bajo nuestro eslogan “COMPROMISO DE INTEGRIDAD”, concepto que ha sido fundamental en la incursión de toda la familia del grupo Midhco, fundada por el matrimonio conyugal Vergara Eizaguirre.</p>
                                 </div>
